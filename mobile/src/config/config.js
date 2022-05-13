db908fefd038809bfe4b55c91796a920b61ced11
yahiachames <yahiachames@gmail.com>
2022-05-13 17:41:22 +0100

    hello

--- mobile/src/config/config.js
+++ mobile/src/config/config.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = "http://192.168.1.104:1999/";
+export const SERVER_URL = "http://192.168.1.72:1999/";
 export const APP_NAME = "Xpress Drive";
 export const API_URL = SERVER_URL + "api/";
 export const GOOGLE_MAPS_KEY = "AIzaSyDVkCvEx5QMLFAYx45LGWX7XlM5_yC8MJI";
